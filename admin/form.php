18681fcc3e5f8624572f87d21b45dfca53d18b41
GroundP <bloom0819@naver.com>
2021-04-19 15:44:20 +0000

    Modify css for admin

--- admin/form.php
+++ admin/form.php
@@ -1,126 +1,128 @@
-<?php  
-
-	$con = mysqli_connect("localhost", "root", "8077", "healing_tent");
-	$sql    = "SELECT c.name, c.phone_number, c.start_time, c.end_time, c.profit, p.package_name FROM client as c, package as p where c.package_id = p.id;";
-	$result = mysqli_query($con, $sql);
-
-	$aClientNames = [];
-	$aClientPhones = [];
-	$aClientPackages = [];
-	$aClientStart = [];
-	$aClientEnd = [];
-	$aClientPrice = [];
-	while($row = mysqli_fetch_array($result))
-	{
-		$aClientNames[] = $row["name"];
-		$aClientPhones[] = $row["phone_number"];
-		$aClientStart[] = $row["start_time"];
-		$aClientEnd[] = $row["end_time"];
-		$aClientPrice[] = $row["profit"];
-		$aClientPackages[] = $row["package_name"];
-	}
-
-	mysqli_close($con);
-
-   	$con = mysqli_connect("localhost", "root", "8077", "healing_tent");
-    $sql    = "select * from stock;";
-    $result = mysqli_query($con, $sql);
-
-	$aStockNames = [];
-	$aStockId = [];
-	$aStockQuantity = [];
-    while($row = mysqli_fetch_array($result))
-    {
-		$aStockNames[] = $row["item"];
-		$aStockId[] = $row["id"];
-		$aStockQuantity[] = $row["quantity"];
-    }
-
-    mysqli_close($con);
-
-
-	$con = mysqli_connect("localhost", "root", "8077", "healing_tent");
-    $sql    = "select * from package;";
-    $result = mysqli_query($con, $sql);
-
-	$aPkgNames = [];
-	$aPkgId = [];
-	$aPkgWeekdayPrice = [];
-	$aPkgWeekendPrice = [];
-    while($row = mysqli_fetch_array($result))
-    {
-		$aPkgNames[] = $row["package_name"];
-		$aPkgId[] = $row["id"];
-		$aPkgWeekdayPrice[] = $row["weekday_price"];
-		$aPkgWeekendPrice[] = $row["weekend_price"];
-    }
-
-    mysqli_close($con);
-?>
 <section>
-	<div id="main_content">
-		<div id="join_box">
-		<form name="admin_form" method="post" action="admin_modify.php?id=<?=$userid?>">
-		<h2 id = admin_box>관리자 모드 > 손님 관리</h2>
-	    <ul id="member_list">
-			<?php
-			for($i = 0; $i < count($aClientNames); $i++)
-			{
-				?>
-				<li>
-				<div class="col1"><?=$aClientNames[$i]?></div>
-				<div class="col2"><?=$aClientPhones[$i]?></div>
-				<div class="col3"><?=$aClientPackages[$i]?></div>
-				<div class="col4"><?=$aClientStart[$i]?></div>
-				<div class="col5"><?=$aClientEnd[$i]?></div>
-				<div class="col6"><?=$aClientPrice[$i]?></div>
-				</li>
-			<?php  
-			}
-			?>
-	    </ul>
-	
-			<h2 id = admin_box>관리자 모드 > 재고 관리</h2>
-	   		<ul id="member_list">
-			<?php
-			for($i = 0; $i < count($aStockId); $i++)
-			{
-				?>
-				<div class="col1"><?=$aStockNames[$i]?></div>
-				<div class="col2">
-					<input type="text" name="stock" value="<?=$aStockQuantity[$i]?>">
-				</div>
-			<?php  
-			}
-			?>
-	    	</ul>
+	<div id="admin_box">
+		<h3 id = member_title>관리자 모드 > 손님 관리</h3>
+	    <ul id="client_list">
+		<li>
+			<span class="col1">이름</span>
+			<span class="col2">전화번호</span>
+			<span class="col3">패키지</span>
+			<span class="col4">시작시간</span>
+			<span class="col5">종료시간</span>
+			<span class="col6">가격</span>
+			<span class="col7">수정</span>
+			<span class="col8">삭제</span>
+		</li>	
 
-			<h2 id = admin_box>관리자 모드 > 패키지 관리</h2>
-	   		<ul id="member_list">
-			<?php
-			for($i = 0; $i < count($aPkgId); $i++)
-			{
-				?>
-				<div class="col1"><?=$aPkgNames[$i]?></div>
-				<div class="col2">
-					<input type="text" name="weekday" value="<?=$aPkgWeekdayPrice[$i]?>">
-				</div>
-				<div class="col3">
-					<input type="text" name="weekend" value="<?=$aPkgWeekendPrice[$i]?>">
-				</div>
-				</div>
-			<?php  
-			}
-			?>
-	    	</ul>
 
-				<div class="buttons">
-					<img style="cursor:pointer" src="../img/button_save.gif" onclick="check_input()">&nbsp;
-					<img id="reset_button" style="cursor:pointer" src="../img/button_reset.gif"
-						onclick="reset_form()">
-				</div>
+<?php
+$con = mysqli_connect("localhost", "root", "8077", "healing_tent");
+$sql    = "SELECT c.name, c.phone_number, c.start_time, c.end_time, c.profit, p.package_name FROM client as c, package as p where c.package_id = p.id;";
+$result = mysqli_query($con, $sql);
+
+while($row = mysqli_fetch_array($result))
+{
+	$aClientName = $row["name"];
+	$aClientPhone = $row["phone_number"];
+	$aClientPackage = $row["package_name"];
+	$aClientStart = $row["start_time"];
+	$aClientEnd = $row["end_time"];
+	$aClientPrice = $row["profit"];
+?>
+		<li>
+			<form name="admin_form" method="post" action="admin_modify.php?id=<?=$userid?>">
+			<span class="col1"><?=$aClientName?></span>
+			<span class="col2"><?=$aClientPhone?></a></span>
+			<span class="col3"><?=$aClientPackage?></span>
+			<span class="col4"><?=$aClientStart?></span>
+			<span class="col5"><?=$aClientEnd?></span>
+			<span class="col6"><?=$aClientPrice?></span>
+			<span class="col7"><button type="submit">수정</button></span>
+			<span class="col8"><button type="button" onclick="location.href='admin_member_delete.php?num=<?=$num?>'">삭제</button></span>
+		</li>
+<?php
+   }
+?>
+		</ul>
+		<h3 id = "stock_title">
+			관리자 모드 > 재고 관리
+		</h3>
+		<ul id="stock_list">
+		<li class="title">
+			<span class="col1">ID</span>
+			<span class="col2">재고 이름</span>
+			<span class="col3">수량</span>
+			<span class="col4">수정</span>
+			<span class="col5">삭제</span>
+		</li>
+		<form method="post" action="admin_board_delete.php">
+<?php
+$con = mysqli_connect("localhost", "root", "8077", "healing_tent");
+$sql    = "select * from stock;";
+$result = mysqli_query($con, $sql);
+
+while($row = mysqli_fetch_array($result))
+{
+	$aStockId = $row["id"];
+	$aStockName = $row["item"];
+	$aStockQuantity = $row["quantity"];
+?>
+		<li>
+			<span class="col1"><?=$aStockId?></span>
+			<span class="col2"><?=$aStockName?></span>
+			<span class="col3"><?=$aStockQuantity?></span>
+			<span class="col4"><button type="submit">수정</button></span>
+			<span class="col5"><button type="button" onclick="location.href='admin_member_delete.php?num=<?=$num?>'">삭제</button></span>
+		</li>
+<?php
+   }
+?>	
+		</ul>
+		<h3 id = "package_title">
+			관리자 모드 > 패키지 관리
+		</h3>
+		<ul id="package_list">
+		<li class="title">
+			<span class="col1">ID</span>
+			<span class="col2">패키지 이름</span>
+			<span class="col3">시간</span>
+			<span class="col4">주중 가격</span>
+			<span class="col5">주말 가격</span>
+			<span class="col6">수정</span>
+			<span class="col7">삭제</span>
+		</li>
+		<form method="post" action="admin_board_delete.php">
+<?php
+$con = mysqli_connect("localhost", "root", "8077", "healing_tent");
+$sql    = "select * from package;";
+$result = mysqli_query($con, $sql);
+
+while($row = mysqli_fetch_array($result))
+{
+	$aPkgId = $row["id"];
+	$aPkgNames = $row["package_name"];
+	$aPkgTime = $row["time"];
+	$aPkgWeekdayPrice = $row["weekday_price"];
+	$aPkgWeekendPrice = $row["weekend_price"];
+?>
+		<li>
+			<span class="col1"><?=$aPkgId?></span>
+			<span class="col2"><?=$aPkgNames?></span>
+			<span class="col3"><?=$aPkgTime?></span>
+			<span class="col4"><?=$aPkgWeekdayPrice?></span>
+			<span class="col5"><?=$aPkgWeekendPrice?></span>
+			<span class="col6"><button type="submit">수정</button></span>
+			<span class="col7"><button type="button" onclick="location.href='admin_member_delete.php?num=<?=$num?>'">삭제</button></span>
+		</li>
+<?php
+   }
+?>	
+		</ul>
+			<div class="buttons">
+				<img style="cursor:pointer" src="../img/button_save.gif" onclick="check_input()">&nbsp;
+				<img id="reset_button" style="cursor:pointer" src="../img/button_reset.gif"
+					onclick="reset_form()">
+			</div>
 		</form>
-		</div> <!-- join_box -->
 	</div> <!-- main_content -->
 </section> 
 
